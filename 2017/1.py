62eeb5bbb703632d19fa52e0d5de809ed12a33bb
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-02 16:23:28 +0100

    this was my first version to show part1 and part2 as well (I refactored this without commiting it first)

--- 2017/1.py
+++ 2017/1.py
@@ -6,24 +6,19 @@ def main():
     # day1_puzzle = "91212129"
 
     puzzle_len = len(day1_puzzle)
-    sums = {
-        "part1": {
-            "step": 1,
-            "sum": 0
-        },
-        "part2": {
-            "step": int(puzzle_len / 2),
-            "sum": 0
-        }
-    }
+    half_way = int(puzzle_len / 2)
+    sum_part1 = sum_part2 = 0
 
     for i in range(puzzle_len):
-        for part, data in sums.items():
-            next_index = (i + data["step"]) % puzzle_len
-            if (day1_puzzle[i] == day1_puzzle[next_index]):
-                data["sum"] += int(day1_puzzle[i])
+        next_index = (i + 1) % puzzle_len
+        if (day1_puzzle[i] == day1_puzzle[next_index]):
+            sum_part1 += int(day1_puzzle[i])
 
-    print(sums["part1"]["sum"], sums["part2"]["sum"])
+        next_index = (i + half_way) % puzzle_len
+        if (day1_puzzle[i] == day1_puzzle[next_index]):
+            sum_part2 += int(day1_puzzle[i])
+
+    print(sum_part1, sum_part2)
 
 
 if __name__ == '__main__':
