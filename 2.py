bf1519899217fcc13fe30ebf4f32edb124f9cc1b
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-02 13:08:18 +0100

    solution for day2 part2

--- 2.py
+++ 2.py
@@ -3,15 +3,26 @@ from common import get_input
 
 def main():
     spreadsheet = get_input("2_input", func=int)
-    checksum = calc_checksum_of_sheet(spreadsheet)
-    print(checksum)
+    checksum_dist, checksum_div = calc_checksum_of_sheet(spreadsheet)
+    print(checksum_dist, checksum_div)
 
 
 def calc_checksum_of_sheet(spreadsheet):
-    checksum = 0
+    checksum_dist = checksum_div = 0
     for row in spreadsheet:
-        checksum += get_row_dist_checksum(row)
-    return checksum
+        checksum_dist += get_row_dist_checksum(row)
+        checksum_div += get_row_div_checksum(row)
+    return (checksum_dist, checksum_div)
+
+
+def get_row_div_checksum(row):
+    for i in range(len(row)):
+        for j in range(i + 1, len(row)):
+            if (row[i] % row[j] == 0):
+                return int(row[i] / row[j])
+            elif (row[j] % row[i] == 0):
+                return int(row[j] / row[i])
+    return 0
 
 
 def get_row_dist_checksum(row):
@@ -20,10 +31,9 @@ def get_row_dist_checksum(row):
     for col in row:
         if (row_max is None):
             row_max = row_min = col
-
-        if col > row_max:
+        elif col > row_max:
             row_max = col
-        if col < row_min:
+        elif col < row_min:
             row_min = col
 
     return row_max - row_min
