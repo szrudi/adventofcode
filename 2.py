4359a44e4966ab868d6d74074dc5f64aeee0f55d
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-02 12:22:22 +0100

    refact: extract methods in 2.py

--- 2.py
+++ 2.py
@@ -3,24 +3,32 @@ from common import get_input
 
 def main():
     spreadsheet = get_input("2_input")
-    checksum = 0
-
-    for row in spreadsheet:
-        row_max = None
-        row_min = None
-        for col in row:
-            col_num = int(col)
-            if (row_max is None):
-                row_max = col_num
-                row_min = col_num
-
-            if col_num > row_max:
-                row_max = col_num
-            if col_num < row_min:
-                row_min = col_num
-        checksum += row_max - row_min
-
+    checksum = calc_checksum_of_sheet(spreadsheet)
     print(checksum)
+
+
+def calc_checksum_of_sheet(spreadsheet):
+    checksum = 0
+    for row in spreadsheet:
+        checksum += get_row_checksum(row)
+    return checksum
+
+
+def get_row_checksum(row):
+    row_max = None
+    row_min = None
+    for col in row:
+        col_num = int(col)
+        if (row_max is None):
+            row_max = col_num
+            row_min = col_num
+
+        if col_num > row_max:
+            row_max = col_num
+        if col_num < row_min:
+            row_min = col_num
+
+    return row_max - row_min
 
 
 if __name__ == '__main__':
