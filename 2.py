6eed7a4143b2fc92e1be818d6c412d453090d11f
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-02 11:18:10 +0100

    day 2 part1 solved

--- 2.py
+++ 2.py
@@ -1,9 +1,26 @@
-import common
+from common import get_input
 
 
 def main():
-    input = common.get_input("2_input")
-    print(input)
+    spreadsheet = get_input("2_input")
+    checksum = 0
+
+    for row in spreadsheet:
+        row_max = None
+        row_min = None
+        for col in row:
+            col_num = int(col)
+            if (row_max is None):
+                row_max = col_num
+                row_min = col_num
+
+            if col_num > row_max:
+                row_max = col_num
+            if col_num < row_min:
+                row_min = col_num
+        checksum += row_max - row_min
+
+    print(checksum)
 
 
 if __name__ == '__main__':
