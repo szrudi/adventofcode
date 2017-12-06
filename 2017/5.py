ecc1c1ee7a1609574ec7bb2bec4ac9bdcc40c107
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-06 22:23:37 +0100

    solution to day5/part1

--- 2017/5.py
+++ 2017/5.py
@@ -1,9 +1,25 @@
-import common
+from common import get_input
 
 
 def main():
-    data = common.get_input('5_input')
+    data = flatten_list(get_input('5_input', func=int))
+    # data = [0, 3, 0, 1, -3]
+    steps = 0
+    index = 0
+    try:
+        while True:
+            # print(data)
+            data[index] += 1
+            index += data[index] - 1
+            steps += 1
+    except IndexError:
+        pass
     print(data)
+    print(steps)
+
+
+def flatten_list(data):
+    return [item for sublist in data for item in sublist]
 
 
 if __name__ == '__main__':
