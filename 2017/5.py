9346925b033031158df0e8e2ad13f6b8207464ba
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-06 17:50:56 +0100

    do one redistribution

--- 2017/5.py
+++ 2017/5.py
@@ -2,7 +2,14 @@ from common import get_input
 
 
 def main():
-    memory_banks = get_input("5_input", 0)
+    memory_banks = get_input("5_input", 0, func=int)
+    print(memory_banks)
+    block_count = max(memory_banks)
+    block_index = memory_banks.index(block_count)
+    memory_banks[block_index] = 0
+    for i in range(block_count):
+        index = (block_index + 1 + i) % (len(memory_banks))
+        memory_banks[index] += 1
     print(memory_banks)
 
 
