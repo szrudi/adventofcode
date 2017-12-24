c0173bb1d508da98f8863b8a1d1cc4f611fcd380
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-24 13:30:49 +0100

    day23/2 simplify

--- 2017/23_input_opt.py
+++ 2017/23_input_opt.py
@@ -4,42 +4,34 @@ a = 1
 c = b = 99
 
 if a != 0:
-    b *= 100
-    b -= -100000
-    c = b
-    c -= -17000
+    c = b = (b * 100) + 100000
+    c += 17000
 while True:
     f = 1
     d = 2
     while True:
         e = 2
         while True:
-            g = d
-            g *= e
-            g -= b
+            g = (d * e) - b
             if g == 0:
                 f = 0
-            e -= -1
-            g = e
-            g -= b
-            # print("4:",a, b, c, d, e, f, g, h)
+            e += 1
+            g = e - b
+            # print("4:", a, b, c, d, e, f, g, h)
             if g == 0:
                 break
-        d -= -1
-        g = d
-        g -= b
+        d += 1
+        g = d - b
         print("3:", a, b, c, d, e, f, g, h)
         if g == 0:
             break
     if f == 0:
-        h -= -1
-    g = b
-    g -= c
+        h += 1
+    g = b - c
     print("2:", a, b, c, d, e, f, g, h)
     if g == 0:
         break
-    b -= -17
+    b += 17
     print("1:", a, b, c, d, e, f, g, h)
-
 
 print(a, b, c, d, e, f, g, h)
