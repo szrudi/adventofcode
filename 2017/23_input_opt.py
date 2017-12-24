0e7d35c047d1f584d319340a0007629c37a71af0
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-24 14:20:34 +0100

    day23/2 some more opt.

--- 2017/23_input_opt.py
+++ 2017/23_input_opt.py
@@ -12,24 +12,19 @@ while True:
     while True:
         e = 2
         while True:
-            g = (d * e) - b
-            if g == 0:
+            if (d * e) == b:
                 f = 0
             e += 1
-            g = e - b
-            # print("4:", a, b, c, d, e, f, g, h)
-            if g == 0:
+            if e == b:
                 break
         d += 1
-        g = d - b
         print("3:", a, b, c, d, e, f, g, h)
-        if g == 0:
+        if d == b:
             break
     if f == 0:
         h += 1
-    g = b - c
-    print("2:", a, b, c, d, e, f, g, h)
-    if g == 0:
+    # print("2:", a, b, c, d, e, f, g, h)
+    if b == c:
         break
     b += 17
     print("1:", a, b, c, d, e, f, g, h)
