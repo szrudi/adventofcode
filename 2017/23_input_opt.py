5f661f0217e444502b320b9d748780d8b5ed1575
Rudi Szabo <rudi.szabo@codecool.com>
2017-12-24 14:28:48 +0100

    day23/2 restructure loops

--- 2017/23_input_opt.py
+++ 2017/23_input_opt.py
@@ -6,27 +6,38 @@ c = b = 99
 if a != 0:
     c = b = (b * 100) + 100000
     c += 17000
-while True:
+
+for b in range(b, c+1, 17):
     f = 1
-    d = 2
-    while True:
-        e = 2
-        while True:
+    for d in range(2, b + 1):
+        for e in range(2, b + 1):
             if (d * e) == b:
                 f = 0
-            e += 1
-            if e == b:
-                break
-        d += 1
         print("3:", a, b, c, d, e, f, g, h)
-        if d == b:
-            break
     if f == 0:
         h += 1
-    # print("2:", a, b, c, d, e, f, g, h)
-    if b == c:
-        break
-    b += 17
-    print("1:", a, b, c, d, e, f, g, h)
+
+# while True:
+#     f = 1
+#     # d = 2
+#     # while True:
+#     #     # e = 2
+#     #     # while True:
+#     #     #     if (d * e) == b:
+#     #     #         f = 0
+#     #     #     e += 1
+#     #     #     if e == b:
+#     #     #         break
+#     #     d += 1
+#     #     print("3:", a, b, c, d, e, f, g, h)
+#     #     if d == b:
+#     #         break
+#     if f == 0:
+#         h += 1
+#     # print("2:", a, b, c, d, e, f, g, h)
+#     if b == c:
+#         break
+#     b += 17
+#     print("1:", a, b, c, d, e, f, g, h)
 
 print(a, b, c, d, e, f, g, h)
