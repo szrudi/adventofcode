0803ea2f5e7db7ad0e9b3e9a87fa21531612b044
Rudi Szabó <szrudi@gmail.com>
2020-12-01 10:26:11 +0100

    day1 solved

---
+++ 2020/1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream('1-input.txt');
+    const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
+    let data = [];
+    for await (const line of rl) {
+        data.push(parseInt(line));
+    }
+    return data;
+}
+
+processLineByLine().then(data => {
+    for (let i = 0; i < data.length; i++) {
+        for (let j = data.length - 1; j >= i; j--) {
+            if (data[i] + data[j] === 2020) {
+                console.log("Multiple of two:", data[i], data[j], data[i] * data[j]);
+            }
+            for (let k = i; k < j; k++) {
+                if (data[i] + data[j] + data[k] === 2020) {
+                    console.log("Multiple of three:", data[i], data[j], data[k], data[i] * data[j] * data[k]);
+                }
+            }
+        }
+    }
+});
