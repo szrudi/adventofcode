03ae855f88d2b08955328ee00cf1e1b8b2d74413
Rudi Szabó <szrudi@gmail.com>
2020-12-02 09:12:26 +0100

    day 2

---
+++ 2020/2.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream('2-input.txt');
+    const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
+    let data = [];
+    for await (const line of rl) {
+        // 5-11 r: bhvgmszrpgrgwcdp
+        const capturingRegex = /(?<min>\d+)-(?<max>\d+) (?<letter>\w): (?<password>.*)/;
+        const found = line.match(capturingRegex);
+        // console.log(found.groups);
+        data.push(found.groups);
+    }
+    return data;
+}
+
+processLineByLine().then(rules => {
+    let validPasswordCount = 0;
+    let validPasswordCount2 = 0;
+    for (let rule of rules) {
+        let letterCount = {};
+        let splitPassword = rule.password.split('');
+        for (const passwordLetter of splitPassword) {
+            if (letterCount.hasOwnProperty(passwordLetter)) {
+                letterCount[passwordLetter]++;
+            } else {
+                letterCount[passwordLetter] = 1;
+            }
+        }
+        if (
+            letterCount[rule.letter] >= parseInt(rule.min) &&
+            letterCount[rule.letter] <= parseInt(rule.max)
+        ) {
+            validPasswordCount++;
+        }
+        if (
+            splitPassword[rule.min - 1] === rule.letter ^
+            splitPassword[rule.max - 1] === rule.letter
+        ) {
+            validPasswordCount2++;
+        }
+        // console.log(letterCount);
+    }
+    console.log(validPasswordCount);
+    console.log(validPasswordCount2);
+});
