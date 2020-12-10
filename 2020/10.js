2f438b7bcc5b5007c5857ef0f28c330d662cef7d
Rudi Szabó <szrudi@gmail.com>
2020-12-10 11:12:14 +0100

    day 10 part 2 not quick enough

--- 2020/10.js
+++ 2020/10.js
@@ -14,16 +14,53 @@ async function processLineByLine() {
     return data;
 }
 
-processLineByLine().then(allNumbers => {
-    allNumbers.sort((a, b) => a - b);
-    // console.log(allNumbers);
-    let previousNumber = 0;
-    let differanceCounts = {1: 0, 2: 0, 3: 1};
+processLineByLine().then(numbers => {
+    numbers.sort((a, b) => a - b);
+    numbers.unshift(0);
+    numbers.push(numbers[numbers.length - 1] + 3)
+    // console.log(numbers);
+    let previousNumber = null;
+    let differenceCounts = {1: 0, 2: 0, 3: 0};
 
-    for (let number of allNumbers) {
-        differanceCounts[number - previousNumber]++;
+    for (let number of numbers) {
+        differenceCounts[number - previousNumber]++;
         previousNumber = number;
     }
-    console.log(differanceCounts);
-    console.log(differanceCounts[1], "*", differanceCounts[3], "=", differanceCounts[1] * differanceCounts[3]);
+    console.log(differenceCounts);
+    console.log(differenceCounts[1], "*", differenceCounts[3], "=", differenceCounts[1] * differenceCounts[3]);
+
+    let allPossibilities = [{adapters: [], indexToCheck: 0}];
+    while (true) {
+        const runningThreads = allPossibilities.filter(e => e.indexToCheck < numbers.length - 1);
+        if (runningThreads <= 0) {
+            break;
+        }
+
+        for (let thread of runningThreads) {
+            const currentIndex = thread.indexToCheck;
+            const adapter = numbers[currentIndex];
+            let validAdapterCount = 0;
+            for (let j = 1; j <= 3; j++) {
+                const nextIndex = currentIndex + j;
+                if (nextIndex >= numbers.length) {
+                    break;
+                }
+                const nextAdapter = numbers[nextIndex];
+                if (nextAdapter - adapter > 3) {
+                    break;
+                }
+                // console.log(adapter, "-->", nextAdapter);
+                validAdapterCount++;
+                if (validAdapterCount > 1) {
+                    thread = JSON.parse(JSON.stringify(thread));
+                    allPossibilities.push(thread);
+                }
+                thread.indexToCheck = nextIndex;
+                thread.adapters.push(nextAdapter);
+            }
+            // console.log(validAdapters);
+        }
+    }
+    console.log(allPossibilities.length);
 });
+
