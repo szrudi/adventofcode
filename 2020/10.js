02fe8b8f809d0bd9c134e506e3090c0b763efa92
Rudi Szabó <szrudi@gmail.com>
2020-12-10 14:59:13 +0100

    day 10 part 2 done

--- 2020/10.js
+++ 2020/10.js
@@ -19,6 +19,11 @@ processLineByLine().then(numbers => {
     numbers.unshift(0);
     numbers.push(numbers[numbers.length - 1] + 3)
     // console.log(numbers);
+    firstPart(numbers);
+    secondPart(numbers);
+});
+
+function firstPart(numbers) {
     let previousNumber = null;
     let differenceCounts = {1: 0, 2: 0, 3: 0};
 
@@ -28,39 +33,45 @@ processLineByLine().then(numbers => {
     }
     console.log(differenceCounts);
     console.log(differenceCounts[1], "*", differenceCounts[3], "=", differenceCounts[1] * differenceCounts[3]);
+}
 
-    let allPossibilities = [{adapters: [], indexToCheck: 0}];
-    while (true) {
-        const runningThreads = allPossibilities.filter(e => e.indexToCheck < numbers.length - 1);
-        if (runningThreads <= 0) {
-            break;
-        }
+function secondPart(numbers) {
+    let possibleSteps = calculatePossibleSteps(numbers);
+    console.log(possibleSteps);
+    console.log("All possible arrangements: ", sumTree(possibleSteps, 0));
+}
 
-        for (let thread of runningThreads) {
-            const currentIndex = thread.indexToCheck;
-            const adapter = numbers[currentIndex];
-            let validAdapterCount = 0;
-            for (let j = 1; j <= 3; j++) {
-                const nextIndex = currentIndex + j;
-                if (nextIndex >= numbers.length) {
-                    break;
+function calculatePossibleSteps(numbers) {
+    let possibleSteps = numbers.map(adapter => {
+        return {adapter, validNextIndexes: [], numberOfPaths: null}
+    })
+    for (let index = 0; index < possibleSteps.length; index++) {
+        for (let j = 1; j <= 3; j++) {
+            let adapter = possibleSteps[index].adapter;
+            let nextIndex = index + j;
+            if (nextIndex < possibleSteps.length) {
+                let nextAdapter = possibleSteps[nextIndex].adapter;
+                if (nextAdapter - adapter <= 3) {
+                    // console.log(adapter, "-->", nextAdapter);
+                    possibleSteps[index].validNextIndexes.push(nextIndex)
                 }
-                const nextAdapter = numbers[nextIndex];
-                if (nextAdapter - adapter > 3) {
-                    break;
-                }
-                // console.log(adapter, "-->", nextAdapter);
-                validAdapterCount++;
-                if (validAdapterCount > 1) {
-                    thread = JSON.parse(JSON.stringify(thread));
-                    allPossibilities.push(thread);
-                }
-                thread.indexToCheck = nextIndex;
-                thread.adapters.push(nextAdapter);
             }
-            // console.log(validAdapters);
         }
     }
-    console.log(allPossibilities.length);
-});
+    return possibleSteps;
+}
 
+function sumTree(steps, index) {
+    let currentStep = steps[index];
+    if (currentStep.validNextIndexes.length === 0) {
+        currentStep.numberOfPaths = 1;
+    }
+    if (currentStep.numberOfPaths === null) {
+        for (let nextStepIndex of currentStep.validNextIndexes) {
+            currentStep.numberOfPaths += sumTree(steps, nextStepIndex);
+            // console.log(steps);
+        }
+    }
+
+    return currentStep.numberOfPaths;
+}
