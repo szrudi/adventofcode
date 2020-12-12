8e052d59a59f3ff4cbed08a18c91d124fb9b9d84
Rudi Szabó <szrudi@gmail.com>
2020-12-12 09:58:31 +0100

    use adventDay in days 1, 2, 3, 11

--- 2020/2.js
+++ 2020/2.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
 
 async function processLineByLine() {
-    const fileStream = fs.createReadStream('2-input.txt');
+    const adventDay = path.basename(__filename).split(".")[0];
+    const fileStream = fs.createReadStream(adventDay + '-input.txt');
     const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
     let data = [];
     for await (const line of rl) {
