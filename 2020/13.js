6dee4e9335bf83d045970500a86e13d5477ccae5
Rudi Szabó <szrudi@gmail.com>
2020-12-13 17:00:43 +0100

    Day 13 part 2 not quick enough

--- 2020/13.js
+++ 2020/13.js
@@ -8,7 +8,7 @@ Number.prototype.mod = function (n) {
 
 async function processLineByLine() {
     const adventDay = path.basename(__filename).split(".")[0];
-    const fileStream = fs.createReadStream(adventDay + '-input.txt');
+    const fileStream = fs.createReadStream(adventDay + '-input1.txt');
     const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
     let data = [];
     for await (const line of rl) {
@@ -21,13 +21,48 @@ async function processLineByLine() {
 }
 
 processLineByLine().then(data => {
+    // data.buses = [17,"x",13,19];
+    // data.buses = [67, 7, 59, 61];
+    // data.buses = [67,"x",7,59,61];
+    // data.buses = [67,7,"x",59,61];
+    // data.buses = [1789,37,47,1889];
+
     let arrivals = new Map();
-    for (let bus of data.buses) {
+    let constraints = new Map();
+    for (let [index, bus] of data.buses.entries()) {
         if (bus === "x") {
             continue;
         }
         bus = parseInt(bus);
         arrivals.set(bus, bus - data.departTime.mod(bus));
+        constraints.set(bus, index);
     }
     console.log("arrivals:", arrivals);
+    console.log("constraints:", constraints);
+
+    let onlyBuses = data.buses.filter(b => b !== "x");
+    onlyBuses.sort((a, b) => b - a);
+    // console.log(onlyBuses);
+
+    const maxBus = Math.max(...onlyBuses);
+    let time = 1;
+    while (time++) {
+        let currentTime = time * maxBus - constraints.get(maxBus);
+        if (time % 10000000 === 0) {
+            console.log(currentTime, time * maxBus, time);
+        }
+        if (isValid(currentTime, onlyBuses, constraints)) {
+            console.log(currentTime);
+            break;
+        }
+    }
 });
+
+function isValid(time, buses, constraints) {
+    for (let bus of buses) {
+        if ((time + constraints.get(bus)).mod(bus) !== 0) {
+            return false
+        }
+    }
+    return true;
+}
